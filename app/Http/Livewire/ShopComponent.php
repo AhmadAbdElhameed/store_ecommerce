2289f42d7324e12a3d7093bbb4e984ebc2f311de
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-01-14 18:48:07 +0200

    Add to cart

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -5,14 +5,21 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Product;
-
+use Cart;
 
 class ShopComponent extends Component
 {
     use WithPagination;
 
+    public function store($product_id , $product_name , $product_price){
+
+        Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
+        session()->flash('success_message','Item added to your cart');
+        return redirect()->route('shop.cart');
+    }
     public function render()
     {
+
         $products = Product::paginate(12);
         return view('livewire.shop-component',['products' => $products]);
     }
