f4bf9624bce56f88a4cfc72b3d4f487533f709e2
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-01 17:16:47 +0200

    fix search term in search box to still exist

--- app/Http/Livewire/HeadSearchComponent.php
+++ app/Http/Livewire/HeadSearchComponent.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class HeadSearchComponent extends Component
 {
+    public  $q;
+    public function mount(){
+        $this->fill(request()->only('q'));
+    }
     public function render()
     {
         return view('livewire.head-search-component');
