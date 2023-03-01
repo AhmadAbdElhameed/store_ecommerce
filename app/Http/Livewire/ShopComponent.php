bf70db335d7a294dafc29450d6bb9f721b82d4d3
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-01 17:34:06 +0200

    remove products from wishlist

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -40,6 +40,15 @@ class ShopComponent extends Component
         Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate("App\Models\Product");
         $this->emitTo('wishlist-icon-component','refreshComponent');
     }
+    public function removeFromWishList($product_id){
+        foreach(Cart::instance('wishlist')->content() as $witem){
+            if($witem->id == $product_id){
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-icon-component','refreshComponent');
+                return;
+            }
+        }
+    }
 
     public function render()
     {
