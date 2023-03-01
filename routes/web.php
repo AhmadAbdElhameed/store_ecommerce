a18b710f5bc131b1d8c6f96294bca18724c2e278
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-01 17:55:52 +0200

    showing all wishlist products

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
+use App\Http\Livewire\WishlistComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
@@ -47,6 +48,7 @@ Route::get('/cart',CartComponent::class)->name('shop.cart');
 Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
 Route::get('/product-category/{slug}',CategoryComponent::class)->name('product.category');
 Route::get('/search', SearchComponent::class)->name('product.search');
+Route::get('/wishlist', WishlistComponent::class)->name('shop.wishlist');
 
 
 
