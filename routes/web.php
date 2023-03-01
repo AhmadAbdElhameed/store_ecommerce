108af7e55b65c95735f66b1552225a255f3f20fe
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-01 15:00:08 +0200

    search products

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
@@ -45,6 +46,10 @@ Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
 Route::get('/cart',CartComponent::class)->name('shop.cart');
 Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
 Route::get('/product-category/{slug}',CategoryComponent::class)->name('product.category');
+Route::get('/search', SearchComponent::class)->name('product.search');
+
+
+
 
 
 Route::middleware(['auth'])->group(function(){
