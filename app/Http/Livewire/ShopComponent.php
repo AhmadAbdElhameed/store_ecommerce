d0072ba7202fb72333faf4fd109f5ac5558dc560
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-01 16:54:51 +0200

    add products to wishlist

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -18,8 +18,9 @@ class ShopComponent extends Component
 
     public function store($product_id , $product_name , $product_price){
 
-        Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
+        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
         session()->flash('success_message','Item added to your cart');
+        $this->emitTo('cart-icon-component','refreshComponent');
         return redirect()->route('shop.cart');
     }
 
@@ -35,6 +36,10 @@ class ShopComponent extends Component
 
     }
 
+    public function addToWishList($product_id,$product_name,$product_price){
+        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate("App\Models\Product");
+        $this->emitTo('wishlist-icon-component','refreshComponent');
+    }
 
     public function render()
     {
