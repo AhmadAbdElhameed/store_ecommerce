e3f973ca8bbddd1ebaacb9d913ff14d702b9895c
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-05 16:14:49 +0200

    edit product feature has added

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
+use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
@@ -70,6 +71,7 @@ Route::middleware(['auth','authadmin'])->group(function(){
     Route::get('/admin/category/edit/{category_id}',AdminEditCategoryComponent::class)->name('admin.category.edit');
     Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
     Route::get('/admin/products/add',AdminAddProductComponent::class)->name('admin.products.add');
+    Route::get('/admin/products/edit/{product_id}',AdminEditProductComponent::class)->name('admin.products.edit');
 
 });
 
